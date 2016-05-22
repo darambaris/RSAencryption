e4fcbf74dccf833bd315b5eca08eb8eb9ddf8919
Jéssika Darambaris <jessika.darambaris@gmail.com>
2016-05-22 07:41:10 -0300

    Algoritmo Knapsack implementado juntamente com o RSA :D:D

--- criptografia.py
+++ criptografia.py
@@ -1,15 +1,16 @@
 import random
+import binascii
 from math import sqrt
 from math import pow
 from array import array
-PMINBITS = pow(10,6);
-PMAXBITS = pow(10,8);
+PMINBITS = 100
+PMAXBITS = 1000
 
 
 # funcao que gera um numero primo aleatorio muito grande 
 def randomPrimeNumber():
 	#PROBLEMA PARTE1
-	number = random.randint(100,1000)
+	number = random.randint(PMINBITS,PMAXBITS)
 	
 	while True:
 		if (int(number%2) == 0):
@@ -43,7 +44,8 @@ def MDC(a, b):
 def randomKeyPublic(FI):
 	while True:
 		# depois ver como fica o role da chave
-		eKeyPublic = random.randint(65537,(FI-1));
+		# 65537,(FI-1)
+		eKeyPublic = random.randint(2,20);
 		if (MDC(FI,eKeyPublic) == 1):
 			break
 	
@@ -54,7 +56,7 @@ def encryptRSA(text,e,n):
 	textEncrypted = []
 	for i in xrange(0,len(text)):
 		
-		textEncrypted.append(ord(text[i]))
+		textEncrypted.append(text[i])
 		
 
 		k = 1
@@ -87,7 +89,6 @@ def privateKey(e,FI,const):
 
 
 def decryptedRSA(privateKey,textEncrypted,n):
-	textDecrypted = []
 	aux = []
 	
 	for i in xrange(0,len(textEncrypted)):
@@ -100,53 +101,113 @@ def decryptedRSA(privateKey,textEncrypted,n):
 			k = k%n
 			j += 1
 
-		aux[i] = int(k)
-		textDecrypted.append(chr(aux[i])) 
+		aux[i] = int(k) 
 		
-	return textDecrypted
-"""
+	return aux
+
 def calculateW(w):
-	for (i in range(0,7)):
-		resultW = keyW[i]
+	resultW = 0
+	for i in xrange(0,8):
+		resultW += w[i]
 	return resultW	
 
 def BetaPublicKey(w,q,r):
 	Bkey = []
-	for (i in range(0,7)):
-		Bkey[i] = mod(w[i]*r,q)
+	for i in xrange(0,8):
+		Bkey.append(mod(w[i]*r,q))
 
 	return Bkey
-"""	
-""" 
-    keyW = [2,7,11,21,42,89,180,354]
-    
-    sumW = calculateW(keyW)
 
-    keyQ = random.randint((sumW+1),sumW*10)
-    keyR = random.randint(1,(sumW-1))
-    keyB = BetaPublicKey(keyW,keyQ, keyR)
-"""
+
+
+def tobits(s):
+    result = []
+    for c in s:
+        bits = bin(ord(c))[2:]
+        bits = '00000000'[len(bits):] + bits
+        result.extend([int(b) for b in bits])
+    return result
+
+def frombits(bits):
+    chars = []
+    for b in range(len(bits) / 8):
+        byte = bits[b*8:(b+1)*8]
+        chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
+    return ''.join(chars)
+	
+def encryptKnapSack (keyB,text):
+	binary = []
+	sumBinary = 0
+	textEncrypted = []
+	for i in xrange(0,len(text)):
+		b = tobits(text[i])
+		sumBinary = 0
+		for j in xrange(0,8):
+			sumBinary += keyB[j]*int(b[j])
+		textEncrypted.append(sumBinary)
+	return textEncrypted
+
+def maxPosition (vecW,num):
+	pos = 0
+	for i in xrange(0,8):
+		if (vecW[i] <= num and vecW[i]>vecW[pos]): 
+			pos = i
+	return pos		
+
+def decryptKnapSack (r,q,textEncrypted,w):
+	pK = privateKey(r,q,1)
+	textDecrypted = []
+	for i in xrange(0,len(textEncrypted)):
+		aux = mod(textEncrypted[i]*pK,q)
+		print aux
+		b = tobits(chr(0))
+		while (aux > 0):
+			pos = maxPosition(w,aux)
+			print "pos: ",pos
+			b[pos] = 1
+			aux = aux-w[pos]
+			
+		textDecrypted.append(frombits(b))
+
+	return textDecrypted
 
 def main():
-    text = 'the first time that i program in python'
+    text = 'Allan Turing'
     primeP = randomPrimeNumber()
     primeQ = randomPrimeNumber()
 
     nKeyPublic = primeP*primeQ
     functionFI = (primeP - 1)*(primeQ -1)
+ 	
+    keyW = [2,7,11,21,42,89,180,354]
+    
+    sumW = calculateW(keyW)
 
-  
+    #keyQ = random.randint((sumW+1),sumW*10)
+    keyQ = 881
+    #keyR = random.randint(1,(sumW-1))
+    keyR = 588
+    keyB = BetaPublicKey(keyW,keyQ, keyR)
+
+    print keyB
+
     eKeyPublic = randomKeyPublic(functionFI)
+    textEncryptedKnapSack = encryptKnapSack (keyB,text)
 
-	#textEncryptedKnapSack = encryptKnapSack (keyB,text)
+    print textEncryptedKnapSack
+    print privateKey(keyR,keyQ,1)
 
-    textEncrypted = encryptRSA(text,eKeyPublic,nKeyPublic)
+    
+    textEncrypted = encryptRSA(textEncryptedKnapSack,eKeyPublic,nKeyPublic)
     
     dprivateKey = privateKey(eKeyPublic,functionFI,1)
 
   
     textDecrypted = decryptedRSA(dprivateKey,textEncrypted,nKeyPublic)
-   
+    
+    textDecryptedKnapSack = decryptKnapSack (keyR,keyQ,textDecrypted,keyW)
 
-    print textDecrypted 
+    print textDecryptedKnapSack
+
+    
 main()	
